17646385ef4ad4578437b02fb4ba3d65b4b3bb11
GordeYash <gordeyash798@gmail.com>
2025-09-10 10:30:09 +0000

    Added multiply function

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
